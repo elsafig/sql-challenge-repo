3e596bed415477af3180a1abb4ca2c13ed11a8d7
Elsa <116039323+elsafig@users.noreply.github.com>
2023-01-05 22:13:50 -0500

    Update module9queries.sql

--- module9queries.sql
+++ module9queries.sql
@@ -31,7 +31,16 @@ FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%' ;
 
 --List each employee in the Sales department, including their employee number, last name, and first name(2 points) 
-
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name
+FROM ((employees
+INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no)
+	  INNER JOIN dept ON dept_emp.dept_no = dept.dept_no)
+	 WHERE dept.dept_name = 'Sales';
+	 
 --List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name(4 points)
-
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, dept.dept_name
+FROM ((employees
+INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no)
+	  INNER JOIN dept ON dept_emp.dept_no = dept.dept_no)
+	 WHERE dept.dept_name = 'Sales' OR dept.dept_name='Development';
 --List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name)(4 points)
