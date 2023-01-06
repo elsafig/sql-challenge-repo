c239b812982b117735aee533b3d996f824d51a4b
Elsa <116039323+elsafig@users.noreply.github.com>
2023-01-05 22:04:39 -0500

    Update query file to save progress

--- module9queries.sql
+++ module9queries.sql
@@ -11,21 +11,27 @@ FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
 --List the manager of each department along with their 
---department number, department name, employee number, last name, and first name
-SELECT dept_no, dept_name, employees.emp_no, employees.last_name, employees.first_name
+--department number, department name, employee number, last name, and first name(2 points)
+SELECT dept_man.dept_no, dept.dept_name, dept_man.emp_no, employees.last_name, employees.first_name
+FROM ((employees
+INNER JOIN dept_man ON employees.emp_no = dept_man.emp_no)
+	  INNER JOIN dept ON dept_man.dept_no = dept.dept_no);
 
 --List the department number for each employee along with that employee’s 
---employee number, last name, first name, and department name 
---SELECT
+--employee number, last name, first name, and department name(2 points) 
+SELECT dept_emp.dept_no, dept.dept_name, dept_emp.emp_no, employees.last_name, employees.first_name
+FROM ((employees
+INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no)
+	  INNER JOIN dept ON dept_emp.dept_no = dept.dept_no);
 
 --List first name, last name, and sex of 
---each employee whose first name is Hercules and whose last name begins with the letter B
+--each employee whose first name is Hercules and whose last name begins with the letter B(2 points)
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%' ;
 
---List each employee in the Sales department, including their employee number, last name, and first name 
+--List each employee in the Sales department, including their employee number, last name, and first name(2 points) 
 
---List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name
+--List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name(4 points)
 
---List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name)
+--List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name)(4 points)
